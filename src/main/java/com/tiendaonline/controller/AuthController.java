0dc55e3b360b9916e4f2cc723823a370ebcb9524
jcastili <jul_ed@hotmail.com>
2026-06-04 23:34:19 -0500

    chore: eliminar CORS hardcodeado y deshabilitar swagger en produccion

--- src/main/java/com/tiendaonline/controller/AuthController.java
+++ src/main/java/com/tiendaonline/controller/AuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:4200")
+
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
